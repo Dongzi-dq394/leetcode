54d09f6dae6cf9280784b7f8719f83865c31856c
Dongzi Qu <dq394@nyu.edu>
2020-06-18 19:13:06 -0400

    updated

--- python_solution/0007.py
+++ python_solution/0007.py
@@ -2,14 +2,13 @@ class Solution:
     def reverse(self, x: int) -> int:
         # solution 1:
         # this solution is using the built-in function of python
-        '''
         sign = 1
         if x < 0:
             sign = -1
         up, down = (1 << 31) - 1, -(1 << 31)
         result = sign * int(str(abs(x))[::-1])
         return result if down <= result <= up else 0
-        '''
+        
         # solution 2:
         # this solution uses the normal math method
         up, down = (1 << 31) - 1, -(1 << 31)

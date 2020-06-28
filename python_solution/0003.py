6b552f137baf47fbc7920cd7fe710fc72564c408
Dongzi Qu <61256557+Dongzi-dq394@users.noreply.github.com>
2020-06-27 21:45:34 -0400

    Update 0003.py

--- python_solution/0003.py
+++ python_solution/0003.py
@@ -1,7 +1,7 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-		# The first solution
-		if not s: return 0
+	# The first solution
+	if not s: return 0
         maxlen, fast, slow = 1, 0, 0
         slen = len(s)
         dic = {}
@@ -20,13 +20,13 @@ class Solution:
         maxlen = max(maxlen, fast-slow)
         return maxlen
 
-		# The second solution
-		dic = {}
-		maxlength, leftend = 0, 0
-		for i in range(len(s)):
-			if s[i] in dic:
-				if dic[s[i]] >= leftend:
-					leftend = dic[s[i]] + 1
-			dic[s[i]] = i
-			maxlength = max(maxlength, i-leftend+1)
-		return maxlength
+	# The second solution
+	dic = {}
+	maxlength, leftend = 0, 0
+	for i in range(len(s)):
+		if s[i] in dic:
+			if dic[s[i]] >= leftend:
+				leftend = dic[s[i]] + 1
+		dic[s[i]] = i
+		maxlength = max(maxlength, i-leftend+1)
+	return maxlength

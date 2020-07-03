ad6c00fcb0294809d413fbb9c02bf977c97d5a6a
Dongzi Qu <dq394@nyu.edu>
2020-07-02 22:20:31 -0400

    Python Solution Added

---
+++ python_solution/0033.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            middle = (left + right) // 2
+            if nums[middle] == target:
+                return middle
+            else:
+                if nums[middle] >= nums[left]:
+                    if target >= nums[left] and target < nums[middle]:
+                        right = middle - 1
+                    else:
+                        left = middle + 1
+                else:
+                    if target <= nums[right] and target > nums[middle]:
+                        left = middle + 1
+                    else:
+                        right = middle - 1
+        return -1

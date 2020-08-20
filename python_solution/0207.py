2419adc1f883a5b26462c54e4372efa9333c3b6a
Dongzi Qu <dq394@nyu.edu>
2020-08-19 21:18:04 -0400

    Solution Updated

--- python_solution/0207.py
+++ python_solution/0207.py
@@ -1,23 +1,43 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        if len(prerequisites) <= 1: return True
-        
-        In_degree = defaultdict(int)
-        Out_point = defaultdict(list)
-        stack = []
+        # Solution 1: Topological Sort
+        degree = defaultdict(int)
+        edges = defaultdict(list)
+        queue = []
         for item in prerequisites:
-            Out_point[item[0]].append(item[1])
-            In_degree[item[1]] += 1
+            degree[item[0]] += 1
+            edges[item[1]].append(item[0])
         for i in range(numCourses):
-            if In_degree[i] == 0:
-                stack.append(i)
+            if degree[i] == 0:
+                queue.append(i)
+        res = 0
+        while queue:
+            res += 1
+            node = queue.pop(0)
+            for item in edges[node]:
+                degree[item] -= 1
+                if degree[item] == 0:
+                    queue.append(item)
+        return res==numCourses
         
-        nums = 0
-        while stack:
-            point = stack.pop(0)
-            nums += 1
-            for vertex in Out_point[point]:
-                In_degree[vertex] -= 1
-                if In_degree[vertex] == 0:
-                    stack.append(vertex)
-        return nums == numCourses
+        # New Solution 2: DFS (100ms: 87.14%)
+        # Much faster than the previous one, but use more space cuz recursion
+        def dfs(i, visited, edges):
+            if visited[i] == 1:
+                return False
+            if visited[i] == 2:
+                return True
+            visited[i] = 1
+            for vertice in edges[i]:
+                if not dfs(vertice, visited, edges):
+                    return False
+            visited[i] = 2
+            return True
+        visited = [0]*numCourses
+        edges = defaultdict(list)
+        for item in prerequisites:
+            edges[item[1]].append(item[0])
+        for i in range(numCourses):
+            if not dfs(i, visited, edges):
+                return False
+        return True
--- python_solution/0210.py
+++ python_solution/0210.py
@@ -1,19 +1,48 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # Old Solution: Topological Sort (116ms: 55.92%)
         res = []
-        
-        in_degree = defaultdict(int)
-        out_vertex = defaultdict(list)
+        edges = defaultdict(list)
+        degree = [0]*numCourses # This is better than dict
         for item in prerequisites:
-            in_degree[item[0]] += 1
-            out_vertex[item[1]].append(item[0])
-        for vertex in range(numCourses):
-            if in_degree[vertex] == 0:
-                res.append(vertex)
+            edges[item[1]].append(item[0])
+            degree[item[0]] += 1
+        queue = []
+        count = 0
+        for i in range(numCourses):
+            if degree[i] == 0:
+                queue.append(i)
+        while queue:
+            node = queue.pop(0)
+            res.append(node)
+            count += 1
+            for vertice in edges[node]:
+                degree[vertice] -= 1
+                if degree[vertice] == 0:
+                    queue.append(vertice)
+        return res if count==numCourses else []
         
-        for item in res:
-            for vertex in out_vertex[item]:
-                in_degree[vertex] -= 1
-                if in_degree[vertex] == 0:
-                    res.append(vertex)
-        return res if len(res)==numCourses else []
+        # New Solution: from Solution
+        # We can still use DFS
+        def dfs(i, visited, edges):
+            if visited[i] == 1:
+                return False
+            if visited[i] == 2:
+                return True
+            visited[i] = 1
+            for vertice in edges[i]:
+                if not dfs(vertice, visited, edges):
+                    return False
+            visited[i] = 2
+            # We put the deepest node at first and then reverse
+            res.append(i)
+            return True
+        res = []
+        edges = defaultdict(list)
+        visited = [0]*numCourses
+        for item in prerequisites:
+            edges[item[1]].append(item[0])
+        for i in range(numCourses):
+            if not dfs(i, visited, edges):
+                return []
+        return res[::-1] # Reverse is important!

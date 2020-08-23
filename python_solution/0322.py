cd261022c3377f21111bfa8b626c8389c99411e9
Dongzi Qu <dq394@nyu.edu>
2020-08-23 15:28:39 -0400

    Solution Updated

--- python_solution/0322.py
+++ python_solution/0322.py
@@ -1,36 +1,60 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # Solution 1: DP (2376ms: 17.80%) --> can be improved to 1816ms: 40.36%
-        dp = [float('Inf')]*(amount+1)
+        # Solution 1: Basic DP (2172ms: 26.54%)
+        dp = [float("Inf")]*(amount+1)
         dp[0] = 0
-        #for i in range(1, amount+1):
-        #    for value in coins:
-        #        if i-value >= 0:
-        #            dp[i] = min(dp[i], dp[i-value]+1)
-        for value in coins:
-            for i in range(value, amount+1):
-                dp[i] = min(dp[i], dp[i-value]+1)
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] = min(dp[i], dp[i-coin]+1)
         return dp[amount] if dp[amount]!=float('Inf') else -1
         
-        # Solution 2: DFS from discussion (624ms: 95.31%)
-        # Try from the large value
-        coins = sorted(coins, reverse=True)
-        res = amount+1
-        def dfs(index, cur_amount, number):
-            nonlocal res
-            if number + cur_amount//coins[index] > res:
-                return
-            if cur_amount%coins[index] == 0:
-                res = number + cur_amount//coins[index]
-                return
-            # If the index for current coin is the last one and it can't make up
-            # the cur_amount, then there is no possibility for success
-            if index == len(coins)-1:
-                return
-            times = cur_amount//coins[index]
-            remain = cur_amount%coins[index]
-            for i in range(times+1):
-                dfs(index+1, remain+i*coins[index], number+times-i)
-        dfs(0, amount, 0)
-        return -1 if res>amount else res
+        # Solution 2: DP adapt to Memo (2136ms: 28.20%)
+        dp = {}
+        def dfs(money):
+            if money == 0:
+                dp[0] = 0
+                return dp[0]
+            if money in dp:
+                return dp[money]
+            dp[money] = float('Inf')
+            for coin in coins:
+                if coin<=money:
+                    dp[money] = min(dp[money], dfs(money-coin)+1)
+            return dp[money]
+        res = dfs(amount)
+        return res if res!=float('Inf') else -1
         
+        # Solution 3: Backtracking (TLE!!)
+        def backtracking(i, remain):
+            if i==0 and remain==0:
+                return True
+            if i==0 or remain<0:
+                return False
+            for j in range(len(coins)):
+                if backtracking(i-1, remain-coins[j]):
+                    return True
+            return False
+        coins.sort(reverse=True)
+        left, right = amount//coins[0], (amount//coins[-1])+1
+        for i in range(left, right+1):
+            if backtracking(i, amount):
+                return i
+        return -1
+        
+        # Solution 4: DFS (660ms: 96.45%)
+        coins.sort(reverse=True)
+        res = float('Inf')
+        def dfs(index, pre_num, remain):
+            nonlocal res
+            if pre_num+int(remain/coins[index])>res:
+                return
+            if remain%coins[index]==0:
+                res = pre_num + remain//coins[index]
+                return
+            if index==len(coins)-1:
+                return
+            total_num = int(remain/coins[index])
+            for i in range(total_num, -1, -1):
+                dfs(index+1, pre_num+i, remain-coins[index]*i)
+        dfs(0, 0, amount)
+        return res if res!=float('Inf') else -1
